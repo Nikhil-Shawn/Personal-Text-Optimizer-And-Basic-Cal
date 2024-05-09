dcf919081c220ad8933aba5aff4553a7a97f2890
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-05-09 23:23:43 +0200

    first commit

---
+++ Personal-Text-Optimizer-And-Basic-Cal/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState('');
+  const [optimizedText, setOptimizedText] = useState('');
+
+  const optimizeText = () => {
+    const optimized = inputText.toLowerCase().replace(/ /g, '_');
+    setOptimizedText(optimized);
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Nikhil's Optimizer</h1>
+      <div className="card">
+        <input className="inputText"
+        type="text"
+        id='inputText'
+        value={inputText}
+        onChange={handleChange}
+        placeholder="Enter text"
+      />
+        <button className="buttonText" onClick={optimizeText}>
+          Optimize
+        </button>
+        <p>Your optimized text is: " {optimizedText} "</p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count for optimization {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
